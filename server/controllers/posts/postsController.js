9fe2bdeaca4cff9ad22cf577373fec631e988301
leroy-netizen <leroythomas051@gmail.com>
2022-03-30 10:09:37 +0300

    mapping out create and get post routes

--- server/controllers/posts/postsController.js
+++ server/controllers/posts/postsController.js
@@ -1,3 +1,7 @@
 export const getPosts = (req, res) => {
   res.send("get posts");
 };
+
+export const createPost = (req, res) => {
+  res.send("Post created");
+};
--- server/routes/posts/postsRoutes.js
+++ server/routes/posts/postsRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { getPosts } from "../../controllers/posts/postsController.js";
+import { createPost } from "../../controllers/posts/postsController.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.get("/", createPost);
 
 export default router;
