a3dea85caeaef15b9452a51d8b10ec0463d01e64
leroy-netizen <leroythomas051@gmail.com>
2022-03-31 10:24:57 +0300

    implement update post logic on the backend

--- server/controllers/posts/postsController.js
+++ server/controllers/posts/postsController.js
@@ -21,3 +21,16 @@ export const createPost = async (req, res) => {
     res.status(409).json({ message: error.message });
   }
 };
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (monggose.Types.Objectid.isValid(_id))
+    return res.status(404).send("No post with such Id");
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+  res.json(updatedPost);
+};
--- server/routes/posts/postsRoutes.js
+++ server/routes/posts/postsRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { getPosts } from "../../controllers/posts/postsController.js";
 import { createPost } from "../../controllers/posts/postsController.js";
+import { updatePost } from "../../controllers/posts/postsController.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.post("/", createPost);
+router.patch("/:id", updatePost);
 
 export default router;
